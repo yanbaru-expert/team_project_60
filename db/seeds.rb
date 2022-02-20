fb74a44cbc4b080b3acf354b3ce46f68271a53cb
satokenengine <satokenengineer@gmail.com>
2022-02-21 08:16:53 +0900

    db/seeds.rb に texts, movies テーブル再生成を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,8 @@ User.find_or_create_by!(email: email) do |user|
   user.password = password
   puts "ユーザーの初期データインポートに成功しました。"
 end
+
+# texts, movies テーブルを再生成（関連付くテーブルを含む）
+%w[texts movies].each do |table_name|
+  ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name} RESTART IDENTITY CASCADE")
+end
