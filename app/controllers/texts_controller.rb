c13af629400ae87d13685d024785576c6daeca4b
s-nashiro <nvmtest07@gmail.com>
2022-04-12 23:48:32 +0900

    教材ページのタイトル表示設定

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,10 +1,10 @@
 class TextsController < ApplicationController
   def index
     @texts = if params[:genre] == "php"
-              Text.where(genre: Text::PHP_GENRE_LIST)
-            else
-              Text.where(genre: Text::RAILS_GENRE_LIST)
-            end
+               Text.where(genre: Text::PHP_GENRE_LIST)
+             else
+               Text.where(genre: Text::RAILS_GENRE_LIST)
+             end
   end
 
   def show
