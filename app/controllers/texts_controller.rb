6d7852294fcb4dd8d3a9989980395e232912030e
satokenengine <satokenengineer@gmail.com>
2022-05-07 22:27:13 +0900

    php, railsページ分けを有効化

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,12 +1,10 @@
 class TextsController < ApplicationController
   def index
     @texts = if params[:genre] == "php"
-               Text.where(genre: Text::PHP_GENRE_LIST)
+               Text.includes(:read_progresses).where(genre: Text::PHP_GENRE_LIST)
              else
-               Text.where(genre: Text::RAILS_GENRE_LIST)
+               Text.includes(:read_progresses).where(genre: Text::RAILS_GENRE_LIST)
              end
-
-    @texts = Text.includes(:read_progresses)
   end
 
   def show
