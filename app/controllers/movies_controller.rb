b15a926036a192b2cbb50c8d18743b76153d4b58
yuki <yukiiso0105@icloud.com>
2022-05-28 17:20:25 +0900

    movies_controller.rbを修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,6 @@ class MoviesController < ApplicationController
   PER_PAGE = 12
 
   def index
-    @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST).page(params[:page]).per(PER_PAGE).includes(:watch_progresses)
+    movies = Movie.includes(:watch_progresses).order(:created_at).genre_list(params[:genre]).page(params[:page]).per(PER_PAGE)
   end
 end
